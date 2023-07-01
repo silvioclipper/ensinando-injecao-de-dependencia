57fc0021e6ea43aa50eaa95003e5ebef49b01e61
silvadias <luisdias04@hotmail.com>
2023-06-30 22:28:14 -0300

    feat: Implementação dos métodos getter e setter na classe Notificacao.

--- src/Notificacao.php
+++ src/Notificacao.php
@@ -4,10 +4,53 @@ declare(strict_types=1);
 
 namespace App;
 
+/**
+ * Classe responsável por representar uma notificação.
+ */
 class Notificacao
 {
-    public function enviarNotificacao($mensagem)
+    /**
+     * @var string A mensagem da notificação.
+     */
+    private $mensagem;
+
+    /**
+     * Construtor da classe Notificacao.
+     *
+     * @param string $mensagem A mensagem da notificação.
+     */
+    public function __construct(string $mensagem)
     {
-        return 'Conteudo da mensagem';
+        $this->mensagem = $mensagem;
+    }
+
+    /**
+     * Obtém a mensagem da notificação.
+     *
+     * @return string A mensagem da notificação.
+     */
+    public function getMensagem(): string
+    {
+        return $this->mensagem;
+    }
+
+    /**
+     * Define a mensagem da notificação.
+     *
+     * @param string $mensagem A nova mensagem da notificação.
+     */
+    public function setMensagem(string $mensagem): void
+    {
+        $this->mensagem = $mensagem;
+    }
+
+    /**
+     * Envia a notificação.
+     *
+     * @return string A mensagem da notificação enviada.
+     */
+    public function enviarNotificacao(): string
+    {
+        return $this->mensagem;
     }
 }
